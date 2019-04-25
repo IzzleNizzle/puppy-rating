4b95a19821e08d773b8a22d34b9abc380ff0c0e8
Iz P <pehisaac@gmail.com>
2019-04-24 20:44:37 -0600

    Updates

---
+++ precache-manifest.de34ce32668a743f046e03094b9f6e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbcaf66d233d3caf09b33d153fe536d9",
+    "url": "/puppy-rating/index.html"
+  },
+  {
+    "revision": "4c09ae8152537daf9f4b",
+    "url": "/puppy-rating/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "940aea4d7a964e44a8ff",
+    "url": "/puppy-rating/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "4c09ae8152537daf9f4b",
+    "url": "/puppy-rating/static/js/2.3f387bab.chunk.js"
+  },
+  {
+    "revision": "940aea4d7a964e44a8ff",
+    "url": "/puppy-rating/static/js/main.308c7fcd.chunk.js"
+  },
+  {
+    "revision": "56cb8276d3b6751ce1fc",
+    "url": "/puppy-rating/static/js/runtime~main.015ab67a.js"
+  }
+]);
